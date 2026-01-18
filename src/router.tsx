3e1a8420e3e08738e17aee59870cbccdcc5b4c46
urdadx <nerdyshinobi12@gmail.com>
2026-01-18 14:44:00 +0000

    added default components

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,13 @@ export function getRouter() {
   const router = createTanStackRouter({
     routeTree,
     scrollRestoration: true,
+    defaultErrorComponent: () => <div>An error occured</div>,
+    defaultPendingComponent: () => (
+      <div className="flex justify-center min-h-screen text-lg font-semibold">Loading...</div>
+    ),
+    defaultNotFoundComponent: () => (
+      <div className="text-2xl flex justify-center min-h-screen">404 - Page Not Found</div>
+    ),
   });
 
   return router;
