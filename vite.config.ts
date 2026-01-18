ce4c99f15624aad6f04c5714df601cb928295304
urdadx <nerdyshinobi12@gmail.com>
2026-01-18 15:36:55 +0000

    deployment v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
-import viteCompression from "vite-plugin-compression";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
-		// 1. Resolve paths first
 		tsConfigPaths(),
-
-		// 2. Process MDX files BEFORE TanStack Start processes the JS
 		{
 			enforce: "pre",
 			...mdx({
@@ -26,21 +23,10 @@ export default defineConfig({
 				providerImportSource: "@mdx-js/react",
 			}),
 		},
-
-		// 3. Main TanStack Start plugin (This includes Vite-React internally)
+		nitro({
+			preset: "node-server",
+		}),
 		tanstackStart(),
-
-		// 4. Styles
 		tailwindcss(),
-
-		// 5. Build optimizations
-		viteCompression({
-			algorithm: "gzip",
-			ext: ".gz",
-		}),
-		viteCompression({
-			algorithm: "brotliCompress",
-			ext: ".br",
-		}),
 	],
 });
